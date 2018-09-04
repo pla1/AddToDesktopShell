ff85981972d8469b7f6937a55c7ba2f6b2e9c7a2
Patrick L Archibald <patrick.archibald@gmail.com>
2018-09-04 15:09:43 -0400

    Remove semicolon.

--- sw.js
+++ sw.js
@@ -1,13 +1,25 @@
 self.addEventListener('install', function(e) {
   console.log("Service worker. " + JSON.stringify(e));
   e.waitUntil(
-    caches.open('AddToDesktopShell').then(function(cache) {
+    caches.open('v1').then(function(cache) {
       return cache.addAll([
         '/AddToDesktopShell/shell.html',
         '/AddToDesktopShell/js/shell.js',
         '/AddToDesktopShell/images/shell192x192.png',
         '/AddToDesktopShell/images/shell512x512.png'
       ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
     })
   );
 });
