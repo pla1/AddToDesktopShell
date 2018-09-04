ba740edd3c62dfcd95195e62fefcce3bcf0b0d69
Patrick L Archibald <patrick.archibald@gmail.com>
2018-09-04 14:29:32 -0400

    Remove semicolon.

--- sw.js
+++ sw.js
@@ -3,7 +3,6 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('AddToDesktopShell').then(function(cache) {
       return cache.addAll([
-        '/AddToDesktopShell/',
         '/AddToDesktopShell/shell.html',
         '/AddToDesktopShell/js/shell.js',
         '/AddToDesktopShell/images/shell192x192.png',
